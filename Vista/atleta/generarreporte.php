c06879e6c9f7c2a1d03b31ae0bf35dc2aab7f5cc
sapm2000 <52897462+sapm2000@users.noreply.github.com>
2019-10-26 14:18:05 -0400

    dom

--- Vista/atleta/generarreporte.php
+++ Vista/atleta/generarreporte.php
@@ -2,14 +2,13 @@
 
 <?php
 session_start();
-require_once("../dompdf-0.5.1/dompdf_config.inc.php");
+require_once("../dompdf/dompdf_config.inc.php");
 
 $dompdf = new DOMPDF();
-$dompdf->set_paper("A4");
-ob_start();
-include 'mi_template_html_de_pdf.php';
-$html_para_pdf = ob_get_clean();
 $dompdf->load_html($_SESSION['reporte']);
 $dompdf->render(); //este comando renderiza el PDF
-$output = $dompdf->output(); //extrae el contenido renderizado del PDF
-file_put_contents('mipdf.pdf', $output); //guarda el PDF en un fichero llamado mipdf.pdf
+$pdf = $dompdf->output(); //extrae el contenido renderizado del PDF
+$filename= 'nombre.pdf';
+$dompdf->stream($filename,array("attachment"=>0));
+
+$html_para_pdf = ob_get_clean();
