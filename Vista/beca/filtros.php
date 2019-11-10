c9dd1a5b1f81eb0bc54ea71b19c1c5050f03e050
sapm2000 <52897462+sapm2000@users.noreply.github.com>
2019-11-10 14:34:59 -0400

    ok

--- Vista/beca/filtros.php
+++ Vista/beca/filtros.php
@@ -32,7 +32,7 @@ $form.='<hr></hr>';
 
 
 $form.='<td> <a href="filtrogeneral.php?accion=actualizar"><input type="button" value="Reporte General" class="botonmodal" title="Reporte General de las Becas" id="" name=""></a></td>';
-$form.='<td><a href="#"><input type="button" value="Reporte Especifico" class="botonmodal" title="Reporte Especifico de las Becas" id="" name=""></a></td>';
+$form.='<td><a href="filtroespecifico.php?accion=actualizar"><input type="button" value="Reporte Especifico" class="botonmodal" title="Reporte Especifico de las Becas" id="" name=""></a></td>';
 $form.='</tr>';
 $form.='<tr>';
 $form.='<td> <a href="#"><input type="button" value="Reporte General Gloria" class="botonmodal" title="Reporte General de las Becas Gloriosas" id="" name=""></a></td>';
