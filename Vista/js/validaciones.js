255cf0719245962115ef2e60dce0a204a81dc9f5
maryosinger05 <maryorithsinger05@gmail.com>
2019-11-20 22:05:08 -0430

    k

--- Vista/js/validaciones.js
+++ Vista/js/validaciones.js
@@ -95,6 +95,24 @@ function caracteres(e){
          return false;
      }
 }
+function caracteress(e){
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz1234567890";
+    especiales = "8-37-39-46";
+
+    tecla_especial = false
+    for(var i in especiales){
+         if(key == especiales[i]){
+             tecla_especial = true;
+             break;
+         }
+     }
+
+     if(letras.indexOf(tecla)==-1 && !tecla_especial){
+         return false;
+     }
+}
 
 function validateMail(id_Mail)
 {
