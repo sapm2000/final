80c70380dd73c94ba11348fb23f94aa29bb627b2
sapm2000 <52897462+sapm2000@users.noreply.github.com>
2019-11-21 20:30:36 -0400

    d

--- Vista/beca/generar.php
+++ Vista/beca/generar.php
@@ -1,17 +1,9 @@
 <?php
 session_start();
 //echo $_SESSION['cliar'];
-$file = fopen($_SESSION['nombres'], "c") or die ("No se pudo crear el archivo");
+$file = fopen("/home/maryo/Escritorio/".$_SESSION['nombres'].".txt", "c") or die ("No se pudo crear el archivo");
 fwrite($file, "1000436501750071690071178501".$_SESSION['fechas'].$_SESSION['cantidad']."00".$_SESSION['total']);
 fwrite($file, "\r\n");
 fwrite($file, $_SESSION['cliar']);
 fclose($file);
 
-$archivo = fopen($_SESSION['nombres'], "r") or die ("No se pudo abrir archivo");
-//si quiero agregar texto al archivo ya existente
-while(!feof($archivo))
-{
-	$traer = fgets($archivo);
-	$salto = nl2br($traer);
-	echo $salto;
-}
