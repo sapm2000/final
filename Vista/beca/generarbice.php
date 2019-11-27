d0a1276171d4147df1ad01137011d30be413e331
sapm2000 <52897462+sapm2000@users.noreply.github.com>
2019-11-26 23:41:09 -0400

    Update generarbice.php

--- Vista/beca/generarbice.php
+++ Vista/beca/generarbice.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 //echo $_SESSION['cliar'];
-$file = fopen("/home/maryo/Escritorio/".$_SESSION['nombres'].'BICENTENARIO'."txt", "c") or die ("No se pudo crear el archivo");
+$file = fopen("/home/maryo/Escritorio/".$_SESSION['nombres'].'BICENTENARIO'.".txt", "c") or die ("No se pudo crear el archivo");
 fwrite($file, "1000436501750071690071178501".$_SESSION['fechas'].$_SESSION['cantidadbice']."00".$_SESSION['totalbice']);
 fwrite($file, "\r\n");
 fwrite($file, $_SESSION['bicen']);
